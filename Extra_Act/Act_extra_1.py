9c6012bfff70466a5147f42d7e86b03b2c73b594
Valeria Del Savio <valeria.delsavio@hotmail.com>
2023-01-16 11:36:13 -0300

    Lesson1 activities added

---
+++ Extra_Act/Act_extra_1.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 16 2023
+
+@author: Vale
+"""
+
+nota_1 = int(input("Enter the first score: "))
+nota_2 = int(input("Enter the second score: "))
+nota_3 = int(input("Enter the third score: "))
+
+final = (nota_1*20 + nota_2*30 + nota_3*50)/100
+
+print("The final score is",final)
