dddae50a21401c64e84dbbf4c7b5c57c30025870
Valeria Del Savio <valeria.delsavio@hotmail.com>
2023-03-14 12:15:13 -0300

    clase 17 config

--- Clase/coderhouse_django/mi_primer_mvt/urls.py
+++ Clase/coderhouse_django/mi_primer_mvt/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from hola_mundo.views import saludar
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('hola-mundo/saludar', saludar),
 ]
